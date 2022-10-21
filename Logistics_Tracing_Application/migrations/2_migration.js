d81a1a619820b5b047598877065eaac9418b5600
Pradeepkumar C Gopai <68938968+pradeep-c-gopai@users.noreply.github.com>
2022-10-21 19:57:02 +0530

    Delete Logistics_Tracing_Application directory

--- Logistics_Tracing_Application/migrations/2_migration.js
+++
@@ -1,5 +0,0 @@
-const Seller = artifacts.require("Seller");
-
-module.exports = async function (deployer) {
-  deployer.deploy(Seller,"0x3FA2C45968D36f025782184Bd6f762BFEEff02b1");
-};
--- Logistics_Tracing_Application/migrations/3_migration.js
+++
@@ -1,5 +0,0 @@
-const Buyer = artifacts.require("Buyer");
-
-module.exports = async function (deployer) {
-  deployer.deploy(Buyer);
-};
